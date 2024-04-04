8111e99f3b8131110f5962292e5b53b8d5ed497d
alberto.mrtnz97@gmail.com <alberto.mrtnz97@gmail.com>
2024-04-04 12:01:23 +0000

    developing reports

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -8,13 +8,15 @@ o.ordersellingprice - o.ordercostprice as orderprofit,
 o.ordercostprice,
 o.ordersellingprice,
 --from raw customer
+c.customerid,
 c.customername,
 c.segment,
 c.country,
 --from product
 p.category,
 p.productname,
-p.subcategory
+p.subcategory,
+p.productid
 from {{ ref('raw_orders') }} as o
 left join {{ ref('raw_costumer') }} as c 
 on o.customerid = c.customerid
