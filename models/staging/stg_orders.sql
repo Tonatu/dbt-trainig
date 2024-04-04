92874a7b1959bdb7ce2ffbfdc9d3e23bb571a684
alberto.mrtnz97@gmail.com <alberto.mrtnz97@gmail.com>
2024-04-04 14:37:23 +0000

    fixing file naming 'costumer' to 'customer'

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -18,7 +18,7 @@ p.productname,
 p.subcategory,
 p.productid
 from {{ ref('raw_orders') }} as o
-left join {{ ref('raw_costumer') }} as c 
+left join {{ ref('raw_customer') }} as c 
 on o.customerid = c.customerid
 left join {{ ref('raw_product') }} as p
 on o.productid = p.productid
